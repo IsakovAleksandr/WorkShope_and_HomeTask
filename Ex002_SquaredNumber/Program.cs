e0cf77f83b5b0d8af1a6555f8ee6cc5a553b52ac
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-04 01:34:41 +0300

    add Ex002_SquaredNumber

---
+++ Ex002_SquaredNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает число 
+// и выводит его квадрат (число умноженное на себя)
+// Например
+// 4 - 16
+// 3 - 9
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+Console.WriteLine("введите число");
+string stringNumber = Console.ReadLine();
+int number;
+while(!int.TryParse(stringNumber, out number))
+{
+    Console.WriteLine("Ошибка! Введите одно целое число");
+stringNumber = Console.ReadLine().Trim();
+}
+//         Console.WriteLine("введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine($"квадрат числа {number}  равен {result}");
+//Console.WriteLine("квадрат числа " + number + " равен " + result);
+    }
+}
