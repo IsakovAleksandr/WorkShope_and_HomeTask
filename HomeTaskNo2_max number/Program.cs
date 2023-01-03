de5e9bf3861fefd34675ae78d189324a9ee7b723
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-04 02:21:29 +0300

    add HomeTaskNo2_max number

---
+++ HomeTaskNo2_max number/Program.cs
@@ -0,0 +1,18 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("введите первое число");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("введите второе число");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        if (number1 > number2)
+        {
+            Console.WriteLine($"{number1} больше {number2}");
+        }
+        else
+        {
+            Console.WriteLine($"{number2} больше {number1}");
+        }
+    }
+}
