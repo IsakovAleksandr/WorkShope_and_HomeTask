6dbcd880b5bb697c68173c06cfde50fb42078a6e
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-03 23:39:10 +0300

    add Ex001_HelloUser

---
+++ Ex001_HelloUser/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+
+if(username == "Алекс")
+{
+Console.WriteLine("Ура! Наконец то - Алекс");
+}
+else
+{
+Console.WriteLine($"Привет, {username}");
+}
