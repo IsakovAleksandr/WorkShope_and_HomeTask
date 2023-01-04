4e08eb0d130b35894381f889960471f9818e04df
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-04 15:29:05 +0300

    add HomeTaskNo6_ParityDefinition

---
+++ HomeTaskNo6_ParityDefinition/Program.cs
@@ -0,0 +1,20 @@
+﻿
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Дано число ");
+        int number = new Random().Next(1, 100);
+        Console.WriteLine(number);
+        // Генерируем случайное число от 1 до 100 и выводим его на экран
+        if (number % 2 == 0)
+        {
+            Console.WriteLine($"Число {number} чётное");
+        }
+        else
+        {
+            Console.WriteLine($"Число {number} нечётное");
+        }
+        // Проверяем чётность/нечётность и выводим результат на экран
+    }
+}
