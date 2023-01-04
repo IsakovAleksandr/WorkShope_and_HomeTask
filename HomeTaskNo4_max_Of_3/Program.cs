e7afab50fb623071b370e159ffe6f5c2d300ed1a
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-04 12:54:16 +0300

    add HomeTaskNo4_max_Of_3

---
+++ HomeTaskNo4_max_Of_3/Program.cs
@@ -0,0 +1,23 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Дано 3 числа:");
+        int number1 = new Random().Next(1, 100);
+        int number2 = new Random().Next(1, 100);
+        int number3 = new Random().Next(1, 100);
+        Console.WriteLine(number1);
+        Console.WriteLine(number2);
+        Console.WriteLine(number3);
+        int max = number1;
+        if(number2 > max)
+        {
+            max = number2;
+        }
+        if(number3 > max)
+        {
+            max = number3;
+        }
+        Console.WriteLine($"Максимальное число = {max}");
+    }
+}
