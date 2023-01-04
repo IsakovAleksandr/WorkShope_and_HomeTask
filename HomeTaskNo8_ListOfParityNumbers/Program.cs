1c28df711cebfeb9451df555e8864764caa87f92
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-04 16:42:20 +0300

    add HomeTaskNo8_ListOfParityDefinition

---
+++ HomeTaskNo8_ListOfParityNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Дано число ");
+int N = new Random().Next(10, 30);
+Console.WriteLine(N); 
+// Сгенерировали случайное число от 10 до 30
+Console.WriteLine($"Вот список чётных чисел от 1 до {N}:");
+for (int num = 1; num <= N; num++)
+{
+    if (num % 2 == 0) // Вставили условие проверки на чётность в цикл
+    {
+        Console.WriteLine(num);
+    }
+// Вывели на экран список чётных чисел от 1 до N
+}
