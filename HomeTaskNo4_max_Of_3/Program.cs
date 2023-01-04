27fe29dbe56026efc32b31dad2814d1c8a3675f3
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-04 13:54:27 +0300

    add comments to HomeTaskNo4_max_Of_3

--- HomeTaskNo4_max_Of_3/Program.cs
+++ HomeTaskNo4_max_Of_3/Program.cs
@@ -2,13 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Дано 3 числа:");
+        Console.WriteLine("Дано 3 числа:"); // Вывели сообщение "Дано 3 числа"
         int number1 = new Random().Next(1, 100);
         int number2 = new Random().Next(1, 100);
         int number3 = new Random().Next(1, 100);
         Console.WriteLine(number1);
         Console.WriteLine(number2);
         Console.WriteLine(number3);
+        // Свормировали 3 случайных числа методом Random и вывели 
+        //их на экран
         int max = number1;
         if(number2 > max)
         {
@@ -19,5 +21,7 @@
             max = number3;
         }
         Console.WriteLine($"Максимальное число = {max}");
+        //Осуществили проверку на максимум используя оператор 
+        //if и вывели максимум на экеран
     }
 }
