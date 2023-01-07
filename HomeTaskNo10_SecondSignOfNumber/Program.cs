00b22a87c4d60e499180b02cbf520ac4479514e3
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-07 15:13:29 +0300

    add HomeTa skNo10_SecondSignOfNumber

---
+++ HomeTaskNo10_SecondSignOfNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трёхзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number / 100 >= 1 || number / 100 < 10)
+{
+    int num = number / 10 % 10;
+    // int sign = num % 10;
+    // Console.WriteLine($"Второй символ числа {number} - {sign}");
+    Console.WriteLine($"Второй символ числа {number} - {num}");
+}
+else
+{
+    Console.WriteLine("Ошибка! Введите трёхзначное число ");
+}
