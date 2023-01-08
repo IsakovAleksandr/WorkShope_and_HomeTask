713b7aefb80fd6566ef8897e6d4d51e0255e1857
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-08 14:23:08 +0300

    add HomeTaskNo13_ThirdSignOfNumber

---
+++ HomeTaskNo13_ThirdSignOfNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите трёхзначное или двухзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number / 100 >= 1 && number / 100 < 10)
+{
+    int num = number % 100 % 10;
+    Console.WriteLine($"Третий символ числа {number} - {num}");
+}
+else if (number / 10 >= 1 && number / 10 < 10)
+{
+    Console.WriteLine($"Третий символ числа {number} отсутствует");
+}
+else if (number / 10 < 1 || number / 10 >= 100)
+{
+    Console.WriteLine("Ошибка! Введите трёхзначное или двузначное число ");
+}
