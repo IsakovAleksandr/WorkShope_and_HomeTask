94001ac21aa0faccee7155a6eef0dbc456063e37
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-08 14:21:26 +0300

    corrected logic in the HomeTa skNo10_SecondSignOfNumber

--- HomeTaskNo10_SecondSignOfNumber/Program.cs
+++ HomeTaskNo10_SecondSignOfNumber/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите трёхзначное число ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number / 100 >= 1 || number / 100 < 10)
+if (number / 100 >= 1 && number / 100 < 10)
 {
     int num = number / 10 % 10;
     // int sign = num % 10;
