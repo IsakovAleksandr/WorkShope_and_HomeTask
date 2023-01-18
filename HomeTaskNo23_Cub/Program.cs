72878c04a2e88f0907d872c4954cf8d9cdeb1c32
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-18 20:59:23 +0300

    update HomeTaskNo23_Cub

--- HomeTaskNo23_Cub/Program.cs
+++ HomeTaskNo23_Cub/Program.cs
@@ -6,8 +6,7 @@ int N = int.Parse(Console.ReadLine());
 Console.WriteLine($"Cubs of numbers from 1 till {N}:");
 for (int i = 1; i <= N; i++)
 {
-// int result = 1;
-int result = (int)Math.Pow(i, 3);
+    int result = (int)Math.Pow(i, 3);
 
-  Console.WriteLine(result);  
+    Console.WriteLine(result);
 }
