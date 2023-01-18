4973482ecbb68b6758e3c129b24c3bd9d609d740
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-18 21:24:37 +0300

    update Ex007_QtyOfDigits

---
+++ Ex007_QtyOfDigits/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+int Length(int number)
+{
+ int count = 1;
+    while(number / 10 != 0)
+    {
+        number = number / 10;
+        count += 1;
+    }
+    return count;
+
+}
+Console.WriteLine("input a number of any digits");
+int a = int.Parse(Console.ReadLine());
+int result = Length(a);
+Console.WriteLine($"qty of number digits = {result}");
