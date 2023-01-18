f3be9dac2e0cd51e81e062d883940a66bbcf61e0
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-18 20:50:17 +0300

    add HomeTaskNo23_Cub

---
+++ HomeTaskNo23_Cub/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23: Напишите программу, которая принимает 
+//на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Input a number ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($"Cubs of numbers from 1 till {N}:");
+for (int i = 1; i <= N; i++)
+{
+// int result = 1;
+int result = (int)Math.Pow(i, 3);
+
+  Console.WriteLine(result);  
+}
