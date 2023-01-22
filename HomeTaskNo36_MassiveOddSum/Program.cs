83ca32432d21f5318248d6acba9d6ef5a4711991
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-01-22 19:05:58 +0300

    add HomeTaskNo36_MassiveOddSum

---
+++ HomeTaskNo36_MassiveOddSum/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными 
+// числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = GetRandomArray(8, 1, 30);
+Console.WriteLine($" [ {String.Join(", ", array)} ] ");
+
+
+int[] GetRandomArray(int size, int start, int end)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(start, end);
+    }
+
+    return result;
+}
+int summ = 0;
+for (int i = 1; i < array.Length; i+=2)
+{
+    summ += array[i];
+}
+Console.WriteLine(summ);
