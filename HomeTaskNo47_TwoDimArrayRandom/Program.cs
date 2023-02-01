71b8350568a07352e0d52fa815ac3dd5a12634d8
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-02-01 21:10:26 +0300

    add HomeTaskNo47_TwoDimArrayRandom

---
+++ HomeTaskNo47_TwoDimArrayRandom/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный 
+// случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateAray(int row, int col, double minValue, double maxValue)
+{
+    double[,] dArray = new double[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            dArray[i, j] = Math.Round(new Random().NextDouble() * 10 - 10, 1);
+        }
+    }
+    return dArray;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = 3;
+int n = 4;
+double[,] array = CreateAray(m, n, -2, 2);
+PrintArray(array);
