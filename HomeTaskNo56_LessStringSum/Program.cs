b5a419eb91609929d8a6a608071c3b691632efe1
Aleksandr Isakov <isakov_a.s@mail.ru>
2023-02-12 22:40:31 +0300

    add HomeTaskNo56_LessStringSum

---
+++ HomeTaskNo56_LessStringSum/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с 
+// наименьшей суммой элементов. Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер 
+// строки с наименьшей суммой элементов: 1 строка;
+
+int[,] CreateArray(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+// Вывод двумерного массива
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetStringNum (int [,] array)
+{
+    int row = 0;
+    int minSum = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minSum = minSum + array[0,i];
+    }
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i,j];
+        }
+        if (sum < minSum) 
+        {
+            minSum = sum;
+            row = i;
+        }
+    }
+    return row;
+}
+
+int[,] myArray = CreateArray(4, 4, 0, 10);
+PrintArray(myArray);
+System.Console.WriteLine($"String with min sum number is {GetStringNum(myArray)}");
+
